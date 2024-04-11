29fe49a7488cd5e54cc63b20c749dd61b0f41563
Tom-Hao <58838926+Tom-Hao@users.noreply.github.com>
2024-04-11 15:06:49 +0800

    Delete bmpTurn.h

--- bmpTurn.h
+++
@@ -1,9 +0,0 @@
-#ifndef BMP_TURN
-#define BMP_TURN
-#include <stdio.h>
-#include <malloc.h>
-#include <stdlib.h>
-
-void bmpTurn(unsigned char** bmpData, unsigned char** mspData, int bmpSize, int width, int height, int bmpBitCount);
-
-#endif
